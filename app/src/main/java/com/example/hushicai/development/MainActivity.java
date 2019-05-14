8863d8cdaaa27ffbd06b8c54144a6b343a46517e
hushicai <hushicai@gf.com.cn>
2019-05-14 11:03:17 +0800

    initial commit

---
+++ app/src/main/java/com/example/hushicai/development/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.hushicai.development;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "Development Kit";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.develop_settings);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.develop_settings:
+                startDevelopmentActivity();
+                break;
+            default:
+        }
+    }
+
+    /**
+     * 打开开发者模式界面
+     */
+    private void startDevelopmentActivity() {
+        try {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.d(TAG, "startDevelopmentActivity: " + e.getMessage());
+        }
+    }
+
+}
